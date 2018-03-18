acee9aac27be6c85eb53b1a37b66199ec552c592
Sydnie T <sydnietom@gmail.com>
2018-03-18 23:38:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Sydnie"
-var height = "74"
+var height = "60"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(height+1)
